cd27301de22d8c4ebc4a6dc65f681f9f66182939
Benjamin Todts <benjamin.todts@mediahuis.be>
2018-05-24 15:40:45 +0200

    Added BT.RecipeQL.Infrastructure, RecipeType and IngredientType.

---
+++ BT.RecipeQL.Infrastructure/GraphQL/Types/IngredientType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+using BT.RecipeQL.Domain.Model;
+
+namespace BT.RecipeQL.Infrastructure.GraphQL.Types
+{
+    public class IngredientType : ObjectGraphType<Ingredient>
+    {
+        public IngredientType()
+        {
+            Field(i => i.Name, nullable: false);
+            Field(i => i.Description, nullable: true);
+        }
+    }
+}
---
+++ BT.RecipeQL.Infrastructure/GraphQL/Types/RecipeType.cs
@@ -0,0 +1,14 @@
+﻿using BT.RecipeQL.Domain.Model;
+using GraphQL.Types;
+
+namespace BT.RecipeQL.Infrastructure.GraphQL.Types
+{
+    public class RecipeType : ObjectGraphType<Recipe>
+    {
+        public RecipeType()
+        {
+            Field(r => r.Name, nullable: false);
+            Field(r => r.Ingredients, nullable: false);
+        }
+    }
+}
